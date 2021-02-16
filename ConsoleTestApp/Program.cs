80a8f5686ed3af385193eb46932ddc55dc32d1be
Roman Krivonogov <romansdidnotcrucify@gmail.com>
2021-02-16 05:24:06 +0300

    Experiment #7

--- ConsoleTestApp/Program.cs
+++ ConsoleTestApp/Program.cs
@@ -9,19 +9,19 @@ namespace ConsoleTestApp
         {
 
 
-            //PointSource[] sources = new PointSource[] { new PointSource(0.5, Math.PI / 2 + 0.01, Math.PI / 4), new PointSource(0.5, 3 * Math.PI / 2 + 0.01, 3 * Math.PI / 4) };
-            //Model groundTruth = new Model(1.0, 2, sources);
-            //Model prediction = new Model(1.0, 2, groundTruthNormalDerivative: groundTruth.NormalDerivative);
-            //prediction.AzimuthalRanges.Add(new Tuple<double, double>(0.0, 2 * Math.PI));
-            //prediction.PolarRanges.Add(new Tuple<double, double>(0.0, Math.PI));
-            //prediction.SearchForSources();
-
-            PointSource[] sources = new PointSource[] { new PointSource(0.8, 0.0, Math.PI / 2) };
-            Model groundTruth = new Model(1.0, 1, sources);
-            Model prediction = new Model(1.0, 1, groundTruthNormalDerivative: groundTruth.NormalDerivative);
-            prediction.AzimuthalRanges.Add(new Tuple<double, double>(Math.PI - 0.02, Math.PI + 0.02));
-            prediction.PolarRanges.Add(new Tuple<double, double>(Math.PI / 4, 3 * Math.PI / 4));
+            PointSource[] sources = new PointSource[] { new PointSource(0.5, 0.0, Math.PI / 4), new PointSource(0.3, Math.PI, 3 * Math.PI / 8) };
+            Model groundTruth = new Model(1.0, 2, sources);
+            Model prediction = new Model(1.0, 2, groundTruthNormalDerivative: groundTruth.NormalDerivative);
+            prediction.AzimuthalRanges.Add(new Tuple<double, double>(0.0, 2 * Math.PI));
+            prediction.PolarRanges.Add(new Tuple<double, double>(0.0, Math.PI));
             prediction.SearchForSources();
+
+            //PointSource[] sources = new PointSource[] { new PointSource(0.8, 0.0, Math.PI / 2) };
+            //Model groundTruth = new Model(1.0, 1, sources);
+            //Model prediction = new Model(1.0, 1, groundTruthNormalDerivative: groundTruth.NormalDerivative);
+            //prediction.AzimuthalRanges.Add(new Tuple<double, double>(Math.PI - 0.02, Math.PI + 0.02));
+            //prediction.PolarRanges.Add(new Tuple<double, double>(Math.PI / 4, 3 * Math.PI / 4));
+            //prediction.SearchForSources();
 
 
             Console.WriteLine($"\nFinal results:\n");
