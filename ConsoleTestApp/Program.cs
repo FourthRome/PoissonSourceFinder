68c9e9d037fe7d4c7ccd4300111873fbec4dfcca
Roman Krivonogov <romansdidnotcrucify@gmail.com>
2021-02-24 22:50:23 +0300

    Experiment #5

--- ConsoleTestApp/Program.cs
+++ ConsoleTestApp/Program.cs
@@ -70,12 +70,12 @@
             //prediction.PolarRanges.Add(new Tuple<double, double>(0, Math.PI));
 
             // Hemisphere y > 0
-            prediction.AzimuthalRanges.Add(new Tuple<double, double>(0, Math.PI));
-            prediction.PolarRanges.Add(new Tuple<double, double>(0, Math.PI));
+            //prediction.AzimuthalRanges.Add(new Tuple<double, double>(0, Math.PI));
+            //prediction.PolarRanges.Add(new Tuple<double, double>(0, Math.PI));
 
             // Hemisphere y < 0
-            //prediction.AzimuthalRanges.Add(new Tuple<double, double>(Math.PI, 2 * Math.PI));
-            //prediction.PolarRanges.Add(new Tuple<double, double>(0, Math.PI));
+            prediction.AzimuthalRanges.Add(new Tuple<double, double>(Math.PI, 2 * Math.PI));
+            prediction.PolarRanges.Add(new Tuple<double, double>(0, Math.PI));
 
             // Hemisphere z > 0
             //prediction.AzimuthalRanges.Add(new Tuple<double, double>(0, 2 * Math.PI));
