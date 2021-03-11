044e2aa64fe6d7abbba12a3f6235525b0f52edb6
Roman Krivonogov <romansdidnotcrucify@gmail.com>
2021-03-11 12:33:11 +0300

    Saving before trying extension methods (logic is way too distributed now) (WORK IN PROGRESS)

--- Computation/SourceGroup.cs
+++ Computation/SourceGroup.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Computation
+﻿namespace Computation
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
     // Class to abstract away sources' data from learning process
     public class SourceGroup
     {
@@ -39,6 +39,22 @@ namespace Computation
             }
 
             return result / (4 * Math.PI * rho);
+        }
+
+        // Component of component of the loss function's gradient, 1/4
+        private double CommonDerivativeComponent(double rho, double phi, double theta) // TODO: check the safety of the code for arbitrary coordinates
+        {
+            double result = 0.0;
+
+            for (int i = 0; i < SourceAmount; ++i)
+            {
+                result += (Math.Pow(rho, 2) - Math.Pow(Sources[i].Rho, 2)) / Math.Pow(Sources[i].SquareDistanceFrom(rho, phi, theta), 1.5);
+            }
+
+            result /= 4 * Math.PI * rho;
+            result += GroundTruthNormalDerivative(rho, phi, theta);
+            // result *= Math.Sin(theta);  // COMMENTED OUT: probably a mistake
+            return result;
         }
     }
 }
